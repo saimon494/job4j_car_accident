e0b01cb4184ef2c5c73e80a3b12d95e97f2686d6
saimon494 <saimon.494@gmail.com>
2021-07-20 21:52:11 +0300

    2. Регистрация пользователя [#296069], исправление

--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -1,10 +1,9 @@
 package ru.job4j.accident.control;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accident.model.User;
 import ru.job4j.accident.service.UserService;
 
@@ -17,19 +16,24 @@ public class RegControl {
         this.userService = userService;
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolationException() {
+        return "redirect:/reg?error=true";
+    }
+
     @GetMapping("/reg")
-    public String reg(Model model) {
+    public String reg(@RequestParam(value = "error", required = false) String error,
+                      Model model) {
+        String errorMessage = null;
+        if (error != null) {
+            errorMessage = "Пользователь уже существует";
+        }
+        model.addAttribute("errorMessage", errorMessage);
         return "reg";
     }
 
     @PostMapping("/reg")
-    public String save(@ModelAttribute User user, Model model) {
-        String errorMessage;
-        if (userService.findUser(user.getUsername()) != null) {
-            errorMessage = "Пользователь уже существует";
-            model.addAttribute("errorMessage", errorMessage);
-            return "reg";
-        }
+    public String save(@ModelAttribute User user) {
         userService.saveUser(user);
         return "redirect:/login";
     }
